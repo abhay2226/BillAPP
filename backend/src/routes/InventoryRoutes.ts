77254d53559fdf93c78f39a6e3cceacaee269b46
abhay2226 <abi.dixith26@gmail.com>
2026-09-02 14:53:34 +0530

    Cod

--- backend/src/routes/InventoryRoutes.ts
+++ backend/src/routes/InventoryRoutes.ts
@@ -1,12 +1,44 @@
 import { Router } from "express";
-import { 
-    getInventory, 
-    updatePricing 
+
+import {
+    createInventory,
+    getInventory,
+    getInventoryById,
+    updatePricing,
+    updateInventoryQuantity,
+    deactivateInventory
 } from "../controller/InventoryController.js";
 
 const InventoryRouter = Router();
 
-InventoryRouter.get("/store/:storeId", getInventory);
-InventoryRouter.put("/pricing/:id", updatePricing);
+InventoryRouter.post(
+    "/",
+    createInventory
+);
+
+InventoryRouter.get(
+    "/store/:storeId",
+    getInventory
+);
+
+InventoryRouter.get(
+    "/:id",
+    getInventoryById
+);
+
+InventoryRouter.put(
+    "/pricing/:id",
+    updatePricing
+);
+
+InventoryRouter.put(
+    "/quantity/:id",
+    updateInventoryQuantity
+);
+
+InventoryRouter.patch(
+    "/deactivate/:id",
+    deactivateInventory
+);
 
 export default InventoryRouter;
